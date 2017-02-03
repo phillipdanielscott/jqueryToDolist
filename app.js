9946a469aea1447001d852eeec6487776d6fcb00
Phillip  Scott <phillipscott@Phillips-MacBook-Pro.local>
2017-02-03 17:27:54 -0500

    finsihed styling now onto click events

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 $( document ).ready(function() {
 // console.log("this is working!")
 
+
+
+
+////make the list draggable to any part of the screen
 $(function() {
     $("#inputBox").draggable();
 });
-$(function() {
-    $("#theList").draggable();
-});
 
 
+
+
+
+///append list item to the todo list
+
+$("#submitButton").click(function(){
+ var item = $('#TaskInput').val();
+ $(".toDoList").append(item);
+console.log("the submit button is working!");
+})
+
+$("#flip").click(function(){
+console.log("heart button is working");
+$('#inputBox').addClass('animated flipOutX');
+
+})
 
 
 
